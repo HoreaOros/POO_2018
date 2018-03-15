f984f5331cee1976aeab1307007122d4c1d1917a
Horea Oros <horea.oros@gmail.com>
2018-03-15 14:35:10 +0200

    add comments for singleton pattern

--- Metode/Metode/Singleton.cs
+++ Metode/Metode/Singleton.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace Metode
 {
+    // Singleton pattern
     class Singleton
     {
         private static Singleton instance = null;
+        // constructor private ==> clasa nu poate fi instantiata din afara
         private Singleton()
         {
 
         }
 
+        /// <summary>
+        /// Metoda creaza la primul apel o instanta a clasei. 
+        /// Apelurile ulterioare vor intoarce referinta la obiectul creat la primul apel
+        /// </summary>
+        /// <returns></returns>
         public static Singleton getInstance()
         {
             if (instance == null)
