c8dbeb326cc9abdbccd4b6a3f198f3a758869179
Horea Oros <horea.oros@gmail.com>
2018-03-21 09:30:21 +0200

    add struct and enum project

---
+++ ConsoleApp1/ConsoleApp1/DataCalendaristica.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataC
+{
+    public enum LunileAnului
+    {
+        Ian,
+        Feb,
+        Mar,
+        Apr,
+        Mai,
+        Iun,
+        Iul,
+        Aug,
+        Sept,
+        Oct,
+        Nov,
+        Dec
+    }
+    public struct DataCalendaristica
+    {
+        private int zi;
+        private LunileAnului luna;
+        private int an;
+
+        public DataCalendaristica(int z, LunileAnului la, int a)
+        {
+            zi = z;
+            luna = la;
+            an = a;
+        }
+        public void SetZi(int d)
+        {
+            zi = d;
+        }
+
+        internal void SetLuna(LunileAnului lu)
+        {
+            luna = lu;
+        }
+
+        internal void SetAn(int v)
+        {
+            an = v;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataC;
+namespace ConsoleApp1
+{
+    enum Zile
+    {
+        Luni,
+        Marti,
+        Miercuri,
+        Joi,
+        Vineri,
+        Sambata,
+        Duminica
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zile z = Zile.Miercuri;
+
+            switch (z)
+            {
+                case Zile.Luni:
+                    break;
+                case Zile.Marti:
+                    break;
+                case Zile.Miercuri:
+                    break;
+                case Zile.Joi:
+                    break;
+                case Zile.Vineri:
+                    break;
+                case Zile.Sambata:
+                    break;
+                case Zile.Duminica:
+                    break;
+                default:
+                    break;
+            }
+            Zile z2 = (Zile)3;
+            Console.WriteLine(z2);
+
+            DataC.DataCalendaristica dc = new DataC.DataCalendaristica();
+            dc.SetZi(7);
+            dc.SetLuna(LunileAnului.Mai);
+            dc.SetAn(2018);
+            
+
+
+        }
+    }
+}
