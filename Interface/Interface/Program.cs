e866f466c1904c51186375edb15b3ed1dbb3eea6
Horea Oros <horea.oros@gmail.com>
2018-05-23 11:46:13 +0300

    add AtomPlant project

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 34, 57, 111, 9, 42 };
+
+            Print(v);
+
+            Array.Sort(v);
+
+            Print(v);
+
+
+            Array.Sort(v, new SumComparer());
+
+            Print(v);
+        }
+
+        private static void Print(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write("{0} ", v[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Interface/Interface/SumComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Interface
+{
+    internal class SumComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return DigitSum((int)x) - DigitSum((int)y);
+        }
+
+        private int DigitSum(int x)
+        {
+            int suma = 0;
+            while (x > 0)
+            {
+                suma += x % 10;
+                x = x / 10;
+            }
+            return suma;
+        }
+    }
+}
